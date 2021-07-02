81f88cd504d006ecda29c0cd85b32ef28974f8e6
silverline-X <84576570+silverline-X@users.noreply.github.com>
2021-07-02 12:18:33 +0530

    Update phone_book.c

--- phone_book.c
+++ phone_book.c
@@ -178,12 +178,13 @@ void add(char *name, char *phone) {
 void list(FILE *db_file) {
   entry *p = load_entries(db_file);
   entry *base = p;
+  int count=0;
   while (p!=NULL) {
     printf("%-20s : %10s\n", p->name, p->phone);
-    p=p->next;
+    p=p->next;count++;
   }
-  /* TBD print total count */
-  free_entries(base);
+ printf("Total entries : %d",count);
+ free_entries(base);
 }
 
 
